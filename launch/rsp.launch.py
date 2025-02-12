4eb542fb3b8b7c999c71f322610a2be85c3f0840
subashchandraboseanuradha <subahchandrabose2003@gmail.com>
2025-02-12 11:25:58 +0530

    added lidar - setup time sync error - /scan topic error

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -2,7 +2,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import xacro
 
@@ -28,16 +27,12 @@ def generate_launch_description():
     node_joint_state_publisher = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher'
+        name='joint_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
         node_robot_state_publisher,
         node_joint_state_publisher
     ])
