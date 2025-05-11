d4ca31be370917e792e4772bceece8a31d1f3634
subashchandraboseanuradha <subahchandrabose2003@gmail.com>
2025-05-11 13:36:58 +0530

    Add initial configuration files for navigation stack
    - Created `view_bot.rviz` for RViz visualization settings.
    - Added `nav2_params.yaml` for navigation parameters including AMCL, behavior server, and costmaps.
    - Implemented `nav2_bringup.launch.py` to launch the navigation stack with configurable parameters.
    - Developed `navigation_launch.py` to manage lifecycle nodes and composable nodes for the navigation system.

--- launch/nav2_bringup.launch.py
+++ launch/nav2_bringup.launch.py
@@ -117,10 +117,10 @@ def generate_launch_description():
 
     declare_use_composition_cmd = DeclareLaunchArgument(
         'use_composition', default_value='False',
-        description='Whether to use composed bringup')
+        description='Use composed bringup if True')
 
     declare_use_respawn_cmd = DeclareLaunchArgument(
-        'use_respawn', default_value='False',
+        'use_respawn', default_value='True',
         description='Whether to respawn if a node crashes. Applied when composition is disabled.')
 
     declare_log_level_cmd = DeclareLaunchArgument(
